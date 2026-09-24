1b45d41aff16ab634fdb9df63952979f0c5fa1ca
Luis Alejandro Amaya Torres <45154655+salamallecum@users.noreply.github.com>
2026-09-24 11:08:35 -0500

    AAM - Inicialización de proyecto

--- src/main/java/com/pruebaTecnicaTopaz/web/controller/GlobalControllerAdvice.java
+++ src/main/java/com/pruebaTecnicaTopaz/web/controller/GlobalControllerAdvice.java
@@ -2,12 +2,15 @@ package com.pruebaTecnicaTopaz.web.controller;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.pruebaTecnicaTopaz.domain.dto.response.ErrorResponse;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 import static com.pruebaTecnicaTopaz.util.ErrorCatalog.PRUEBA;
 
@@ -22,6 +25,24 @@ public class GlobalControllerAdvice {
         return ErrorResponse.builder()
                 .codigoError(PRUEBA.getCode())
                 .mensaje(PRUEBA.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    //Método encargado de definir la excepción que se lanza cuando hay un error en la validación de los datos del estudiante
+    //@ResponseStatus(HttpStatus.BAD_REQUEST)
+    //@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        //Obtenemos el BindingResult que contiene los errores de validación
+        BindingResult bindingResult = ex.getBindingResult();
+
+        return ErrorResponse.builder()
+                .codigoError(PRUEBA.getCode())
+                .mensaje(PRUEBA.getMessage())
+                .detalles(bindingResult.getFieldErrors()
+                        .stream()
+                        .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                        .collect(Collectors.toList()))
                 .timestamp(LocalDateTime.now())
                 .build();
     }
